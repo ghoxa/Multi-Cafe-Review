fab9e0ad31ffcef70cbf51fbe1c7f7999562902e
ej970 <ej970624@gmail.com>
2021-02-16 16:08:54 +0900

    주석제거

--- Multi-Cafe-Review/Multi-Cafe-Review/src/kr/co/multicafe/common/interceptor/AdminInterceptor.java
+++ Multi-Cafe-Review/Multi-Cafe-Review/src/kr/co/multicafe/common/interceptor/AdminInterceptor.java
@@ -9,31 +9,31 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AdminInterceptor extends HandlerInterceptorAdapter {
-	// preHandle() : 컨트롤러보다 먼저 수행되는 메서드
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//            throws Exception {
-//        // session 객체를 가져옴
-//        HttpSession session = request.getSession();
-//        // login처리를 담당하는 사용자 정보를 담고 있는 객체를 가져옴
-//        Object obj = session.getAttribute("admin");
-//          
-//        if ( obj == null ){
-//            // 로그인이 안되어 있는 상태임으로 로그인 폼으로 다시 돌려보냄(redirect)
-//            response.sendError(4001, "관리자 로그인이 필요합니다.");
-//            return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
-//        }
-//          
-//        // preHandle의 return은 컨트롤러 요청 uri로 가도 되냐 안되냐를 허가하는 의미임
-//        // 따라서 true로하면 컨트롤러 uri로 가게 됨.
-//        return true;
-//    }
-//  
-//    // 컨트롤러가 수행되고 화면이 보여지기 직전에 수행되는 메서드
-//    @Override
-//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-//            ModelAndView modelAndView) throws Exception {
-//        // TODO Auto-generated method stub
-//        super.postHandle(request, response, handler, modelAndView);
-//    }     
+//	 preHandle() : 컨트롤러보다 먼저 수행되는 메서드
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // session 객체를 가져옴
+        HttpSession session = request.getSession();
+        // login처리를 담당하는 사용자 정보를 담고 있는 객체를 가져옴
+        Object obj = session.getAttribute("admin");
+          
+        if ( obj == null ){
+            // 로그인이 안되어 있는 상태임으로 로그인 폼으로 다시 돌려보냄(redirect)
+            response.sendError(4001, "관리자 로그인이 필요합니다.");
+            return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
+        }
+          
+        // preHandle의 return은 컨트롤러 요청 uri로 가도 되냐 안되냐를 허가하는 의미임
+        // 따라서 true로하면 컨트롤러 uri로 가게 됨.
+        return true;
+    }
+  
+    // 컨트롤러가 수행되고 화면이 보여지기 직전에 수행되는 메서드
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        super.postHandle(request, response, handler, modelAndView);
+    }     
 }
