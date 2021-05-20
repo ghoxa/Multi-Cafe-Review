a27d35fb461d0f82a6a2878d8e873bed5763721b
이름 <hotem1234@gmail.com>
2021-05-20 14:40:28 +0900

    지도 infowindow css수정

--- reacttemplate/src/pages/MapContainer.jsx
+++ reacttemplate/src/pages/MapContainer.jsx
@@ -66,14 +66,17 @@ class MapContainer extends React.Component {
       } else {
         mapUrl = axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${mapId}&radius=20000&y=${getlat}&x=${getlon}`, config);
       }
+      const cafeUrl = axios.get(`http://localhost:9090/multicafe/api/cafe`);
 
-      Promise.all([mapUrl])
-        .then(([res]) => {
+      Promise.all([mapUrl, cafeUrl])
+        .then(([res, res2]) => {
           this.setState({
             data: res.data,
+            cafe: res2.data,
             isLoaded: true,
           });
           console.log(this.state.data);
+          console.log(this.state.cafe);
           for (let i = 0; i < this.state.data.documents.length; i++) {
             displayMarker2(this.state.data.documents[i]);
             bounds.extend(new kakao.maps.LatLng(this.state.data.documents[i].y, this.state.data.documents[i].x));
@@ -87,20 +90,29 @@ class MapContainer extends React.Component {
       // 지도에 마커를 표시하는 함수
       function displayMarker2(place) {
         // 마커를 생성하고 지도에 표시
+        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
+          imageSize = new kakao.maps.Size(44, 49), // 마커이미지의 크기입니다
+          imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+          markerPosition = new kakao.maps.LatLng(place.y, place.x); // 마커가 표시될 위치입니다
         let marker = new kakao.maps.Marker({
           map: map,
           position: new kakao.maps.LatLng(place.y, place.x),
+          // image: markerImage, // 마커이미지 설정
         });
 
         // 마커에 클릭이벤트를 등록
         kakao.maps.event.addListener(marker, 'click', function () {
           // 마커를 클릭하면 장소명이 인포윈도우에 표출
+
           infowindow.setContent(
-            '<div style="padding:1px;font-size:12px;">' +
+            '<div style="padding-top:10px;padding-left:20px;font-size:12px;width:200px;height:30px">' +
               place.place_name +
-              '</div><div style="padding:1px;font-size:12px;">' +
+              '</div><div style="padding-top:3px;padding-left:20px;font-size:12px;width:200px;height:30px">' +
               place.phone +
-              '</div><div style="padding:1px;font-size:12px;">' +
+              '</div><div style="padding-left:20px;font-size:12px;width:200px;height:30px">' +
               place.address_name +
               '</div>'
           );
