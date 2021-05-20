ec9bac96cc9de8f17e3513073008ffbbd170d4bf
ej970 <ej970624@gmail.com>
2021-05-20 21:00:24 +0900

    count추가, 좋아요 에러수정

--- Multi-Cafe-Review/Multi-Cafe-Review/src/kr/co/multicafe/dto/Page.java
+++ Multi-Cafe-Review/Multi-Cafe-Review/src/kr/co/multicafe/dto/Page.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Page {
 	private int pageno;
+	private int count;
 	private int start;
 	private int end;
 	private int prev;
@@ -12,9 +13,10 @@ public class Page {
 	private int endPageNum;
 	private List<Menu> list;
 	
-	public Page(int pageno, int start, int end, int prev, int next, int startPageNum, int endPageNum) {
+	public Page(int pageno, int count, int start, int end, int prev, int next, int startPageNum, int endPageNum) {
 		super();
 		this.pageno = pageno;
+		this.count = count;
 		this.start = start;
 		this.end = end;
 		this.prev = prev;
@@ -22,54 +24,77 @@ public class Page {
 		this.startPageNum = startPageNum;
 		this.endPageNum = endPageNum;
 	}
-	
+
 	public int getPageno() {
 		return pageno;
 	}
+
 	public void setPageno(int pageno) {
 		this.pageno = pageno;
 	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
 	public int getStart() {
 		return start;
 	}
+
 	public void setStart(int start) {
 		this.start = start;
 	}
+
 	public int getEnd() {
 		return end;
 	}
+
 	public void setEnd(int end) {
 		this.end = end;
 	}
+
 	public int getPrev() {
 		return prev;
 	}
+
 	public void setPrev(int prev) {
 		this.prev = prev;
 	}
+
 	public int getNext() {
 		return next;
 	}
+
 	public void setNext(int next) {
 		this.next = next;
 	}
-	public List<Menu> getList() {
-		return list;
-	}
-	public void setList(List<Menu> list) {
-		this.list = list;
-	}
+
 	public int getStartPageNum() {
 		return startPageNum;
 	}
+
 	public void setStartPageNum(int startPageNum) {
 		this.startPageNum = startPageNum;
 	}
+
 	public int getEndPageNum() {
 		return endPageNum;
 	}
+
 	public void setEndPageNum(int endPageNum) {
 		this.endPageNum = endPageNum;
+	}
+
+	public List<Menu> getList() {
+		return list;
+	}
+
+	public void setList(List<Menu> list) {
+		this.list = list;
 	}
 	
 }
